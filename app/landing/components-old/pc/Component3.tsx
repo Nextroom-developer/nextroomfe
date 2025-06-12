497502c63afb9f6d7e2dd3d2b209c57e7d163de4
anyl92 <yulim9592@gmail.com>
2025-06-12 23:33:30 +0900

    chore: 기존 랜딩 old파일로 수정

---
+++ app/landing/components-old/pc/Component3.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
+export default function Component3() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
+  const boxVariants = {
+    hidden: {
+      y: 100, // 시작 위치를 아래로 조정합니다.
+      opacity: 0,
+    },
+    visible: {
+      y: 0, // 최종 위치를 원래 위치로 설정합니다.
+      opacity: 1,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  const imgProps = {
+    src: "/images/landing/landing.png",
+    alt: "NEXT ROOM",
+    width: 786,
+    height: 500,
+  };
+  return (
+    <motion.div
+      className="pc-main3"
+      ref={ref}
+      variants={boxVariants}
+      initial="hidden"
+      animate={controls}
+    >
+      <p className="pc-sub-title3">
+        쉽고 빠르게 <br />
+        힌트를 등록하고 수정하세요.
+      </p>
+      <Image {...imgProps} />
+    </motion.div>
+  );
+}
---
+++ app/landing/components-old/pc/Phone.tsx
@@ -0,0 +1,52 @@
+import { useRef } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
+import Image from "next/image";
+
+import "@/(shared)/utils/firebase";
+import useAnalytics from "../../../(shared)/hooks/useAnalytics";
+
+export default function Phone() {
+  const { logEvent } = useAnalytics();
+  logEvent("screen_view", {
+    firebase_screen: "homepage_function_1",
+    firebase_screen_class: "homepage_function_1",
+  });
+  const imgProps = {
+    src: "/images/landing/hint_phone.png",
+    alt: "NEXT ROOM",
+    width: 316,
+    height: 650,
+  };
+
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll();
+
+  const opacityOffset = 500; // 낮은 값으로 설정하여 투명도 변화가 빠르게 일어나도록 함
+
+  const opacity = useTransform(scrollYProgress, (value) => {
+    const elementTop = ref.current?.offsetTop ?? 0;
+    const elementHeight = ref.current?.offsetHeight ?? 0;
+    const progress = (value * 100 - elementTop + elementHeight) / opacityOffset;
+    return Math.max(0, Math.max(0, progress));
+  });
+
+  return (
+    <motion.div
+      className="pc-img-cont"
+      ref={ref}
+      style={{
+        opacity,
+      }}
+    >
+      <Image {...imgProps} />
+      <div className="pc-title7">
+        직관적으로 알 수 있는 시간
+        <p className="pc-sub-title7">
+          플레이가 시작되면 남은 시간을
+          <br />
+          그래프와 함께 제공합니다.
+        </p>
+      </div>
+    </motion.div>
+  );
+}
