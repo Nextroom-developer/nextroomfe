ddd5c02eaefd8a71afb46ecb2d988e4a87a65317
anyl92 <yulim9592@gmail.com>
2025-06-17 23:08:49 +0900

    chore: 모바일 작업중 파일 커밋

--- app/landing/Container.tsx
+++ app/landing/Container.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import "./styles/landing.modules.sass";
-import "./styles/mobile.modules.sass";
 
 import Component1 from "./components/pc/Component1";
 import Component2 from "./components/pc/Component2";
@@ -9,19 +8,13 @@ import Component3 from "./components/pc/Component3";
 import Component4 from "./components/pc/Component4";
 import Component5 from "./components/pc/Component5";
 import Component6 from "./components/pc/Component6";
-import Component1Mobile from "./components/mobile/Component1Mobile";
-import Component2Mobile from "./components/mobile/Component2Mobile";
-import Component3Mobile from "./components/mobile/Component3Mobile";
-import Component4Mobile from "./components/mobile/Component4Mobile";
-import Component5Mobile from "./components/mobile/Component5Mobile";
-import Component6Mobile from "./components/mobile/Component6Mobile";
-import Component7Mobile from "./components/mobile/Component7Mobile";
-import Component9Mobile from "./components/mobile/Component9Mobile";
-import MobileBtn from "./components/mobile/MobileBtn";
 import PcBtn from "./components/pc/PcBtn";
+import MobileBtn from "./components/mobile/MobileBtn";
 import useScreen from "./hooks/useScreen";
 import FooterComponent from "./components/Footer";
 import NavbarComponent from "./components/Navbar";
+import Component2Mobile from "./components/mobile/Component2Mobile";
+import Component4Mobile from "./components/mobile/Component4Mobile";
 
 function LandingPage() {
   const {
@@ -29,49 +22,30 @@ function LandingPage() {
     isLoading,
     showBtn,
     component1Ref,
+    component2Ref,
     component6Ref,
     component4Ref,
   } = useScreen();
 
   return (
-    <div>
+    <>
       {isLoading && (
-        <>
-          <div className="container-wrapper">
-            <NavbarComponent componentRef={component6Ref} isMobile={isMobile} />
-            {/* 조건부 렌더링을 통해 Btn 컴포넌트를 표시 */}
-            {/* buttonProps를 전달하고 ref를 설정하여 DOM 요소를 참조합니다. */}
-            {isMobile ? (
-              <div className="mobile-wrapper">
-                <Component1Mobile ref={component1Ref} />
-                <Component2Mobile />
-                <Component3Mobile />
-                <Component4Mobile />
-                <Component5Mobile />
-                <Component6Mobile />
-                <Component7Mobile ref={component6Ref} />
-
-                {/* <Component8Mobile /> */}
-                <Component9Mobile ref={component4Ref} />
-                {showBtn && <MobileBtn />}
-              </div>
-            ) : (
-              <>
-                <Component1 ref={component1Ref} />
-                <Component2 />
-                <Component3 />
-                <Component4 ref={component4Ref} />
-                <Component5 />
-                <Component6 ref={component6Ref} />
-                {showBtn && <PcBtn />}
-                {/* <Button {...buttonProps}>지금 바로 시작하기</Button> */}
-              </>
-            )}
-          </div>
+        <div className="container-wrapper">
+          <NavbarComponent componentRef={component6Ref} isMobile={isMobile} />
+          <Component1 ref={component1Ref} />
+          {isMobile ? <Component2Mobile ref={component2Ref} /> : <Component2 />}
+          <Component3 />
+          {isMobile ? <Component4Mobile /> : <Component4 ref={component4Ref} />}
+          <Component5 />
+          <Component6 ref={component6Ref} />
+          {/* 조건부 렌더링을 통해 Btn 컴포넌트를 표시 */}
+          {/* buttonProps를 전달하고 ref를 설정하여 DOM 요소를 참조합니다. */}
+          {!isMobile && showBtn && <PcBtn />}
+          {isMobile && <MobileBtn />}
           <FooterComponent />
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
