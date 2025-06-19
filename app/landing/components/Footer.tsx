311fd7161c704a31ea7a16548af88b72de4f4a24
anyl92 <yulim9592@gmail.com>
2025-06-19 16:17:13 +0900

    feat: 랜딩페이지 피드백 반영

--- app/landing/components/Footer.tsx
+++ app/landing/components/Footer.tsx
@@ -8,23 +8,20 @@ function FooterComponent() {
     <footer className="footer">
       <div className="footer-inner">
         <div className="footer-left">
-          <div className="footer-title">넥스트룸</div>
+          <div className="footer-title">넥스트룸(nextroom)</div>
           <div className="footer-copyright">Copyright © 2023 Next room</div>
           <div className="footer-info">
-            사업자등록번호: 487-23-01961 | 대표: 김주환
+            대표: 김주환
             <br />
-            통신판매번호: 2024-서울강남-05436
+            사업자등록번호: 487-23-01961 | 통신판매번호: 2024-서울강남-05436
             <br />
-            문의메일: nextroom.official@gmail.com
-          </div>
-          <div className="footer-links">
             <Link
               href="https://held-notebook-420.notion.site/d7bea4318d754b61999e9cb6179a2f70?source=copy_link"
               target="_blank"
             >
               개인정보처리방침
             </Link>{" "}
-            | 서비스이용약관
+            | 문의메일: nextroom.official@gmail.com
           </div>
         </div>
         <div className="footer-right">
--- app/landing/components/Navbar.tsx
+++ app/landing/components/Navbar.tsx
@@ -22,15 +22,6 @@ function NavbarComponent({
           <div className="logo-nav">
             <div className="logo-nav-item">
               <Link
-                href="https://sponge-wood-68d.notion.site/107febdc0ad180f09f68fc47e1f4fde2"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                넥스트룸 소개
-              </Link>
-            </div>
-            <div className="logo-nav-item">
-              <Link
                 href="https://held-notebook-420.notion.site/134ed57b9c574733b31feab0ea5c36a5"
                 target="_blank"
                 rel="noopener noreferrer"
