b862cf9e7a8bc320314354dafbf420e66b52cbfb
anyl92 <yulim9592@gmail.com>
2025-06-22 16:44:26 +0900

    feat: mobile 기능 소개 애니메이션 기존 코드로 다시 수정

--- app/landing/components/mobile/Component2Mobile.tsx
+++ app/landing/components/mobile/Component2Mobile.tsx
@@ -1,20 +1,51 @@
-import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import "../../styles/snap.modules.sass";
 
 import Phone1 from "./Phone1";
 import Phone2 from "./Phone2";
 import Phone3 from "./Phone3";
 
-import "../../styles/snap.modules.sass";
-
 const Component2Mobile = () => {
-  const fadeInUp = {
-    initial: { opacity: 0, y: 40 },
-    animate: { opacity: 1, y: 0 },
-    transition: { duration: 0.6, ease: "easeOut" },
-  };
+  const [activeSection, setActiveSection] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+  const opacity = useTransform(scrollYProgress, [0.1, 0.12], [0, 1]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!ref.current || typeof ref === "function") return;
+
+      const sectionTop = ref.current.offsetTop;
+      const scrollY = window.scrollY;
+      const sectionHeight = window.innerHeight * 0.5;
+
+      const currentSection = Math.floor(
+        (scrollY - sectionTop + sectionHeight / 2) / sectionHeight
+      );
+
+      setActiveSection(currentSection);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [ref]);
 
   return (
-    <div className="snap-container">
+    <motion.div
+      className="mobile-img-cont"
+      ref={ref}
+      style={{
+        opacity,
+        position: "sticky",
+      }}
+    >
       <div className="mobile-func-box">
         <h1 className="mobile-title2">
           4년간의 현장 경험으로 탄생한 솔루션 <br />
@@ -23,24 +54,14 @@ const Component2Mobile = () => {
         <h4 className="mobile-sub-title2">
           방탈출에만 몰입할 수 있는 특별한 기능을 제공합니다.
         </h4>
-      </div>
 
-      <div className="snap-section">
-        <motion.div {...fadeInUp}>
-          <Phone1 />
-        </motion.div>
+        <div className="mobile-func-img-box">
+          {activeSection <= 0 && <Phone1 key="phone1" />}
+          {activeSection === 1 && <Phone2 key="phone2" />}
+          {activeSection >= 2 && <Phone3 key="phone3" />}
+        </div>
       </div>
-      <div className="snap-section">
-        <motion.div {...fadeInUp}>
-          <Phone2 />
-        </motion.div>
-      </div>
-      <div className="snap-section">
-        <motion.div {...fadeInUp}>
-          <Phone3 />
-        </motion.div>
-      </div>
-    </div>
+    </motion.div>
   );
 };
 export default Component2Mobile;
--- app/landing/components/mobile/Phone3.tsx
+++ app/landing/components/mobile/Phone3.tsx
@@ -1,14 +1,17 @@
+import { useAnimation, motion } from "framer-motion";
+import { forwardRef, useEffect } from "react";
 import Image from "next/image";
 import "@/(shared)/utils/firebase";
 
 import useAnalytics from "../../../(shared)/hooks/useAnalytics";
 
-const Phone3 = () => {
+const Phone3 = forwardRef<HTMLDivElement>((_, ref) => {
   const { logEvent } = useAnalytics();
   logEvent("screen_view", {
     firebase_screen: "homepage_function_3",
     firebase_screen_class: "homepage_function_3",
   });
+
   const imgProps = {
     src: "/images/landing/hint_phone3.svg",
     alt: "NEXT ROOM",
@@ -16,16 +19,59 @@ const Phone3 = () => {
     height: 350,
   };
 
+  const controls = useAnimation();
+  useEffect(() => {
+    const updateOpacity = () => {
+      if (typeof ref !== "function" && ref?.current) {
+        const viewportHeight = window.innerHeight;
+
+        const start = ref.current.offsetTop + viewportHeight * 0.7;
+        const end = ref.current.offsetTop + viewportHeight * 1.8;
+
+        const { scrollY } = window;
+
+        if (scrollY > start && scrollY < end) {
+          const progress = (scrollY - start) / (end - start);
+          const opacity = 1 - progress;
+          controls.start({ opacity: Math.max(0, opacity) });
+        } else if (scrollY <= start) {
+          controls.start({ opacity: 1 });
+        } else if (scrollY >= end) {
+          controls.start({ opacity: 0 });
+        }
+      }
+    };
+    window.addEventListener("scroll", updateOpacity);
+    updateOpacity(); // Call once to set initial value
+
+    return () => {
+      window.removeEventListener("scroll", updateOpacity);
+    };
+  }, [controls, ref]);
+
+  const phoneVariants = {
+    hidden: { y: 50, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+    exit: { y: -50, opacity: 0 },
+  };
   return (
-    <>
+    <motion.div
+      className="mobile-img-cont"
+      ref={ref}
+      variants={phoneVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      transition={{ duration: 0.5 }}
+    >
       <Image {...imgProps} />
       <div className="mobile-func-description">
         각 테마의 독특한 분위기로
         <br />
         배경화면을 커스텀해보세요.
       </div>
-    </>
+    </motion.div>
   );
-};
+});
 
 export default Phone3;
